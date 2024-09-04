10c593e9bf65312ad85eaf39b003b78cc248372d
Andrey Kerges Nascimento <andrey.nascimento2@fatec.sp.gov.br>
2024-09-03 22:18:36 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿double n1, n2, n3;
+
+Console.WriteLine("--- Maior Nota ---");
+
+Console.WriteLine("Digite sua primeira nota: ");
+n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite sua primeira nota: ");
+n2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite sua primeira nota: ");
+n3 = Convert.ToDouble(Console.ReadLine());
+
+if (n1 > n2 && n1 > n3)
+{
+    Console.WriteLine($"A maior nota é: {n1}");
+}
+else if (n2 > n1 && n2 > n3)
+{
+    Console.WriteLine($"A maior nota é: {n2}");
+}
+else if (n3 > n1 && n3 > n2)
+{
+    Console.WriteLine($"A maior nota é: {n3}");
+}
+else
+{
+    Console.WriteLine("As notas são iguais!");
+}
